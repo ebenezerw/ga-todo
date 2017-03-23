19098404baab508d4bd33403a18b8d4218451310
Ebenezer Williams <ebenezerw@me.com>
2017-03-23 12:02:28 -0400

    can view list and delete items

--- Ga-Todo/Ga-Todo/FirstViewController.swift
+++ Ga-Todo/Ga-Todo/FirstViewController.swift
@@ -8,8 +8,39 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var list = ["Build a Swift App", "Build a MEAN app", "Learn React Native", "Complete Final Project"]
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+    
+    
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            self.list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
